279effe6cec982b21e10ce101cf2e5b92735db9d
kchipp <khipp2@gmail.com>
2016-06-17 14:51:23 -0500

    displays genres, shows and movies

--- NetflixCatalogueSystem/View.cs
+++ NetflixCatalogueSystem/View.cs
@@ -13,23 +13,6 @@ namespace NetflixCatalogueSystem
         {
 
         }
-        public void seeAll()
-        {
-            Console.WriteLine("Shows\n");
-            for (int i = 0; i < 5; i++)
-            {
-               Console.WriteLine(tvshows.ShowName);
-                
-            }
-            Console.WriteLine();
-            Console.WriteLine("Movies\n");
-            for (int i = 0; i < 5; i++)
-            {
-
-                //Console.WriteLine(MovieName[i]);
-            }
-            Console.ReadKey();
-        }
     }//class
 }//namespace
 
